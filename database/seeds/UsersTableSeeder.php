3441795869ff4e74a3277f72fe40d89631c7d897
= <=>
2020-02-11 23:33:20 -0400

    Custom Laravel Auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+/*
+*/
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => null,
+                'created_at'     => '2019-09-21 15:31:45',
+                'updated_at'     => '2019-09-21 15:31:45',
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
